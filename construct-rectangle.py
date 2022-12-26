e7861076b110f8ea010e46eb48648d6d199b465c
taalhaataahir0102 <77788288+taalhaataahir0102@users.noreply.github.com>
2022-12-26 21:49:50 +0500

    Create construct-rectangle.py
    Both length and width will start from sq root of that number because any 2 numbers greater than the sq root will always result greater than area.
    Now keep incrementing the length if L*W>area and vice versa.

---
+++ construct-rectangle.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def constructRectangle(self, area):
+        L = int(math.ceil(math.sqrt(area)))
+        W = int(math.floor(math.sqrt(area)))
+        while L*W !=area:
+            if L*W < area:
+                L+=1
+            elif L*W > area:
+                W-=1
+        return [L,W]
