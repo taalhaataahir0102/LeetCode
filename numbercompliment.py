c9ebc18352f4635a259fa888395c53ae70a1d8ab
taalhaataahir0102 <77788288+taalhaataahir0102@users.noreply.github.com>
2022-12-26 18:15:23 +0500

    Create numbercompliment.py
    Link to problem
    https://leetcode.com/problems/number-complement/description/
    Convert the number to binary string using bin() function. It will take O(log(n))
    Loop over the binary string and replace 1's to 0's and vice versa. It will take O(n)
    convert the binary string to integer using int() function. It wil take O(n)
    So the total time complexity will be O(n)

---
+++ numbercompliment.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findComplement(self, num):
+        b = bin(num)[2:]
+        for i in range(len(b)):
+          if b[i] == '0':
+            list1 = list(b)
+            list1[i] = '1'
+            b = ''.join(list1)
+          elif b[i] == '1':
+            list1 = list(b)
+            list1[i] = '0'
+            b = ''.join(list1)
+        return int(b,2)
