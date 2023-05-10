80a22339a74c73674322f7db125925d53b4c365b
taalhaataahir0102 <77788288+taalhaataahir0102@users.noreply.github.com>
2023-05-10 17:46:23 +0500

    Create Power-of-Two.py
    Keep of calculating 2^x until we exceed the number n or 2^x becomes n.
    Beats 72 percent of submissions in runtime

---
+++ Power-of-Two.py
@@ -0,0 +1,10 @@
+class Solution(object):
+  def isPowerOfTwo(self, n):
+    n1 = 0
+    i = 0
+    while n1 < n:
+      n1 = pow(2,i)
+      i+=1
+      if n1 == n:
+        return True
+    return False
