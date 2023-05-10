295334b05e176800a2607cafbdbaefc5bf8d58da
taalhaataahir0102 <77788288+taalhaataahir0102@users.noreply.github.com>
2023-05-10 18:24:01 +0500

    Create Valid-Anagram.py
    Store the count of each letter of 1st string in a dictionary.
    Reduce the count of each letter in the dictionary if the letter is found in second string.
    If the values of the dict are all zeros than return True.
    Beats 93 percent of submissions in time complexity and 9 percent in space complexity.

---
+++ Valid-Anagram.py
@@ -0,0 +1,14 @@
+class Solution(object):
+  def isAnagram(self, s, t):
+    d = {}
+    for i in s:
+      d[i]=0
+    for i in s:
+      d[i]+=1
+    for i in t:
+      if i not in d:
+        return False
+      d[i] -=1
+    if list(d.values()) == [0]*len(set(s)):
+      return True
+    return False
